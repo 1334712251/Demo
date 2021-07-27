2e6762816f103eb49c3868b375747a0b90c48ace
linhao <1334712251@qq.com>
2021-07-27 20:37:51 +0800

    提交结果

--- src/test/example/com/demo5/Test.java
+++ src/test/example/com/demo5/Test.java
@@ -1,14 +1,27 @@
 package test.example.com.demo5;
 
+import java.util.concurrent.TimeUnit;
+
 public class Test {
 
     public static void main(String[] args) {
         Thread thread = new Thread("d") {
             @Override
             public void run() {
+                try {
+                    TimeUnit.MINUTES.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 System.out.println("dddddddddd");
             }
         };
+        try {
+            TimeUnit.MILLISECONDS.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread.setDaemon(true);
         thread.start();
     }
 }
