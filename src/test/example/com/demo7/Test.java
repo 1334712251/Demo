3bf06e2fd5f2d2d9bc34eae3d0c843f37a43eb01
linhao <1334712251@qq.com>
2021-07-28 11:17:33 +0800

    提交结果

--- src/test/example/com/demo7/Test.java
+++ src/test/example/com/demo7/Test.java
@@ -1,27 +1,38 @@
 package test.example.com.demo7;
 
+import java.util.concurrent.TimeUnit;
+
 public class Test {
 
-    static int count=0;
+    static int count = 0;
+
+    static Object obj = new Object();
 
     public static void main(String[] args) {
+
+        long startTime = System.currentTimeMillis();
+
         Thread t1 = new Thread("t1") {
             @Override
             public void run() {
-                for (int i=0;i<10000;i++){
-                    count++;
+                for (int i = 0; i < 10000; i++) {
+                    synchronized (obj) {
+                        count++;
+                    }
                 }
-                System.out.println(Thread.currentThread().getName());
+                System.out.println(this.getName());
             }
         };
 
         Thread t2 = new Thread("t2") {
             @Override
             public void run() {
-                for (int i=0;i<10000;i++){
-                    count--;
+                for (int i = 0; i < 10000; i++) {
+                    synchronized (obj) {
+                        count--;
+                    }
                 }
-                System.out.println(Thread.currentThread().getName());
+                System.out.println(this.getName());
             }
         };
 
@@ -34,6 +45,9 @@ public class Test {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime - startTime + "ms");
         System.out.println(count);
     }
 }
