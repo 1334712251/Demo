3fd106a4d3c6fcf198f08c9e6d4d30d18b5e8959
linhao <1334712251@qq.com>
2021-08-20 21:52:46 +0800

    提交结果

---
+++ JUC/src/main/java/demo/Test1.java
@@ -0,0 +1,43 @@
+package demo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicIntegerArray;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        demo(()->new AtomicIntegerArray(10), (array)->array.length(),(array,index)->array.getAndIncrement(index),(array)-> System.out.println(array));
+    }
+
+    private static <T> void demo(
+            Supplier<T> arraySupplier,
+            Function<T, Integer> lengthFun,
+            BiConsumer<T, Integer> putConsumer,
+            Consumer<T> printConsumer) {
+        List<Thread> ts = new ArrayList<>();
+        T array = arraySupplier.get();
+        int length = lengthFun.apply(array);
+        for (int i = 0; i < length; i++) {
+            // 每个线程对数组作 10000 次操作
+            ts.add(new Thread(() -> {
+                for (int j = 0; j < 10000; j++) {
+                    putConsumer.accept(array, j % length);
+                }
+            }));
+        }
+        ts.forEach(t -> t.start()); // 启动所有线程
+        ts.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }); // 等所有线程结束
+        printConsumer.accept(array);
+    }
+}
